e58a12781c25784cba56135f3d9cc08525a9645b
inesitadivertida02 <101185266+inesitadivertida02@users.noreply.github.com>
2025-05-21 13:13:18 +0100

    Update Station.java

--- ChargeUnity/backend/src/main/java/tqs/ChargeUnity/model/Station.java
+++ ChargeUnity/backend/src/main/java/tqs/ChargeUnity/model/Station.java
@@ -60,56 +60,6 @@ public class Station {
                 "\nChargers: " + chargers.size() + ";" +
                 "\nChargers List:\n" + rChargers;
     }
-    //getters and setters
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    public String getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(String latitude) {
-        this.latitude = latitude;
-    }
-    public String getLongitude() {
-        return longitude;
-    }
-    public void setLongitude(String longitude) {
-        this.longitude = longitude;
-    }
-    public List<Charger> getChargers() {
-        return chargers;
-    }
-    public void setChargers(List<Charger> chargers) {
-        this.chargers = chargers;
-    }
-    public List<Operator> getOperators() {
-        return operators;
-    }
-    public void setOperators(List<Operator> operators) {
-        this.operators = operators;
-    }
-
+  
 
 }
