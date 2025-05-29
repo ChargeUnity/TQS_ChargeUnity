ae62df2e0a0b2bf349655bf9bd83a05f86bd09ab
Ponte348 <ponte@ua.pt>
2025-05-29 23:15:26 +0100

    CH-96 functional tests done

--- ChargeUnity/backend/src/test/java/tqs/ChargeUnity/functional/steps/SearchChargerSteps.java
+++ ChargeUnity/backend/src/test/java/tqs/ChargeUnity/functional/steps/SearchChargerSteps.java
@@ -52,8 +52,34 @@ public class SearchChargerSteps {
             wait.until(ExpectedConditions.elementToBeClickable(By.id("view-profile-button-3")));
         selectDriverButton.click();
 
-        
+        WebElement findStationsButton =
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("find-stations-button")));
+        findStationsButton.click();
     }
 
+    @When("they enter their current location or a destination")
+    public void they_enter_their_current_location_or_a_destination() {
+        WebElement cityRadio =
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("city-radio")));
+        cityRadio.click();
+
+        WebElement cityInput =
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("city-input")));
+        cityInput.sendKeys("Lisbon");
+    
+        WebElement searchButton =
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("search-by-city-btn")));
+        searchButton.click();
+    }
+
+    @Then("a list of available charging stations in that area should be displayed")
+    public void a_list_of_available_charging_stations_in_that_area_should_be_displayed() {
+        WebElement stationsList =
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("station-results-list")));
+        assertNotNull(stationsList);
+        assertTrue(stationsList.isDisplayed());
+
+        assertFalse(stationsList.findElements(By.tagName("li")).isEmpty());
+    }
 
 }
--- ChargeUnity/backend/src/test/resources/tqs/ChargeUnity/features/searchcharger.feature
+++ ChargeUnity/backend/src/test/resources/tqs/ChargeUnity/features/searchcharger.feature
@@ -1,7 +1,7 @@
 Feature: Search for chargers
 
   Scenario: Display available charging stations
-    Given the user has opened the app,
-    When they navigate to the search stations feature,
-    And they enter their current location or a destination,
-    Then a list of available charging stations in that area should be displayed.
+    Given the user has opened the app
+    When they navigate to the search stations feature
+    And they enter their current location or a destination
+    Then a list of available charging stations in that area should be displayed
