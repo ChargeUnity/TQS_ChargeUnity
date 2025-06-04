62bbd2f20013b767d2a89afc76cf149617d60343
Ponte348 <ponte@ua.pt>
2025-06-04 19:45:22 +0100

    CH-300 station was missing charger association

--- ChargeUnity/backend/src/main/java/tqs/ChargeUnity/model/Station.java
+++ ChargeUnity/backend/src/main/java/tqs/ChargeUnity/model/Station.java
@@ -1,12 +1,18 @@
 package tqs.ChargeUnity.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
@@ -24,8 +30,14 @@ public class Station {
   private String latitude;
   private String longitude;
 
-  @ManyToOne @JsonBackReference private Operator operator;
+  @ManyToOne
+  @JsonBackReference
+  private Operator operator;
 
+  @OneToMany(mappedBy = "station", fetch = FetchType.EAGER)
+  @JsonManagedReference
+  private List<Charger> chargers = new ArrayList<>();
+  
   public Station() {}
 
   @Override
