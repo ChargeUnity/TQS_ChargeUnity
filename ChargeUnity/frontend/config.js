fb7fd75a24311348eb7771617573f1fc1c233272
Alexandre Regalado <alexandreregalado@hotmail.com>
2025-06-05 14:08:46 +0100

    patch 2 to dynamic ip in env

--- ChargeUnity/frontend/config.js
+++ ChargeUnity/frontend/config.js
@@ -1,9 +1,9 @@
 const isDevelopment = window.location.hostname === 'localhost';
 
 const CONFIG = {
-    API_URL: isDevelopment 
+    API_URL: process.env.VITE_APP_API_URL || (isDevelopment 
         ? 'http://localhost:8080/api/v1'
-        : 'http://172.18.13.232:8080/api/v1'
+        : 'http://172.18.13.232:8080/api/v1')
 };
 
 export default CONFIG;
